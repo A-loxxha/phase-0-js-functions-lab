7b118efebee05459086ff88baf3c7c2a00e728bd
A_loxxha <charleskush77@gmail.com>
2026-09-25 09:18:31 +0300

    js labs first commit

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+function calculateTax(amount){
+ return amount * 10/100;
+}
 
+function convertToUpperCase(text){
+ return text.toUppercase;
+}
 
+function findMaximum(num1, num2){
+return num1 > num2 ? num1 : num2;
+}
 
+function isPalindrome(word){
+const cleanWord = word.toLowerCase();
+const reversedWord = cleanWord.split('').reverse().join('');
+return cleanWord === reversedWord;
+}
+
+function calculateDiscountedPrice(originalPrice, discounrPercentage){
+const discountAmmount = (originalPrice * discountPercentage)/100;
+return discountAmmount
+}
+
+return 
 
 
 // This is required for the test to function properly  
